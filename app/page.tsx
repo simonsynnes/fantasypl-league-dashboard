fabd70fff257f300b73273833d7b9a5d2c3dc583
Simon Synnes <simonsynnes@gmail.com>
2024-04-28 23:26:04 +0200

    Added gradient background

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import LeagueDashboard from "@/pages/leagueDashboard";
 
 export default function Home() {
-  return <LeagueDashboard />;
+  return (
+    <div className="min-h-screen bg-gradient-to-r from-gray-200 via-white to-gray-300">
+      <LeagueDashboard />;
+    </div>
+  );
 }
