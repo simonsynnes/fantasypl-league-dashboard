176dee0401b71612aa31a15692b7aea57975244d
Simon Synnes <simonsynnes@gmail.com>
2024-05-01 12:54:35 +0200

    Made player injuries view more compact

--- components/InjurySection.tsx
+++ components/InjurySection.tsx
@@ -10,11 +10,14 @@ const InjurySection: React.FC<InjurySectionProps> = ({ players }) => {
   if (players.length === 0) return null;
 
   return (
-    <section aria-labelledby="injury-section-title" className="my-4">
-      <h2 id="injury-section-title" className="text-xl font-bold my-2">
+    <section aria-labelledby="injury-section-title" className="my-4 px-2">
+      <h2
+        id="injury-section-title"
+        className="text-xl font-bold mb-2 text-center"
+      >
         Injured Players
       </h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-2">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2">
         {players.map((player) => (
           <PlayerCard key={player.id} player={player} />
         ))}
